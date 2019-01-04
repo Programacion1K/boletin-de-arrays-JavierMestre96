8996ee2518b1fe63196563feac67ddd02358f872
Javier Santos <javiermestre96@gmail.com>
2019-01-04 11:40:18 +0100

    @migueldavidorrico metodo que devuelve un array con un numero de direcciones pasandole la direccion y el numero de equipos.

--- src/DireccionIP.java
+++ src/DireccionIP.java
@@ -3,12 +3,15 @@ import java.util.Arrays;
 public class DireccionIP {
 
     public static final int LONGITUD_IP=4;
-    private final int[] direccionIP = new int[LONGITUD_IP];
+    private int[] direccionIP = new int[LONGITUD_IP];
     private String direccionIPTexto;
-
 
     public DireccionIP(String direccionIPTexto){
         this.direccionIPTexto=direccionIPTexto;
+    }
+
+    public DireccionIP(int[] direccionIP){
+        this.direccionIP=direccionIP;
     }
 
     public DireccionIP(int primerNumeroIP, int segundoNumeroIP, int tercerNumeroIP, int cuartoNumeroIP){
@@ -18,17 +21,16 @@ public class DireccionIP {
         direccionIP[3]=cuartoNumeroIP;
     }
 
-
     public String getDireccionIPTexto(){
         return direccionIPTexto;
     }
 
     public String infoIP(){
         String salida="";
-        salida+="Dirección IP: "+toString(direccionIP)+"\n";
-        salida+="¿Es dirección de red? "+esDireccionRed()+"\n";
-        salida+="Dirección red: "+direccionRed()+"\n";
-        salida+="Máscara de red: "+mascaraRed()+"\n";
+        salida+="Dirección IP: "+toString()+"\n";
+        salida+="¿Es dirección de red? "+esDireccionRed(direccionIP)+"\n";
+        salida+="Dirección red: "+direccionRed(direccionIP)+"\n";
+        salida+="Máscara de red: "+mascaraRed(direccionIP)+"\n";
         salida+="Clase "+claseIP();
 
         salida+="";
@@ -51,7 +53,7 @@ public class DireccionIP {
         return direccionIP;
     }
 
-    public String mascaraRed(){ ;
+    private String mascaraRed(int[] direccionIP){ ;
         if(claseIP()=='A'){
             return "255.0.0.0";
         }
@@ -64,8 +66,7 @@ public class DireccionIP {
         return "";
     }
 
-
-    public String direccionRed(){ ;
+    private String direccionRed(int[] direccionIP){ ;
         if(claseIP()=='A'){
             return direccionIP[0]+".0.0.0";
         }
@@ -78,7 +79,7 @@ public class DireccionIP {
         return "";
     }
 
-    public char claseIP(){
+    private char claseIP(){
         if(direccionIP[0]<128){
             return 'A';
         }else if(direccionIP[0]<192){
@@ -89,14 +90,28 @@ public class DireccionIP {
         return 'n';
     }
 
-    public boolean esDireccionRed(){
+    private boolean esDireccionRed(int[] direccionIP){
         if(direccionIP[3]==0){
             return true;
         }
         return false;
     }
 
-    public String toString(int[] direccionIP){
+
+    //Método que devuelva un array con n direcciones IP todas en la misma red (que se le pasa como argumento). N puede ser muy grande.
+
+    public static String[] DireccionesIPEnMismaRed(DireccionIP direccionIPInicial, int numeroEquipos){
+        String[] direccionesIPEnMismaRed=new String[numeroEquipos];
+        for(int i=0; i<numeroEquipos;i++){
+            String direccionIP=direccionIPInicial.toString();
+            int numeroEquipo=Integer.parseInt(direccionIP.substring(direccionIP.length()-2))+i;
+            direccionIP=direccionIP.substring(0,direccionIP.length()-2);
+            direccionesIPEnMismaRed[i]=direccionIP+numeroEquipo;
+        }
+        return direccionesIPEnMismaRed;
+    }
+    @Override
+    public String toString(){
         return direccionIP[0]+"."+direccionIP[1]+"."+direccionIP[2]+"."+direccionIP[3];
     }
 }
--- src/Prueba.java
+++ src/Prueba.java
@@ -5,9 +5,12 @@ public class Prueba {
 
         System.out.println("Hola, Mundo");
         DireccionIP ip1;
+        ip1 = new DireccionIP(192,168,3,20);
+        DireccionIP ip2 = new DireccionIP(192,168,3,5);
+        System.out.println(Arrays.toString(DireccionIP.DireccionesIPEnMismaRed(ip1,15)));
 
-        ip1 = new DireccionIP(192,168,0,5);
-        System.out.println(ip1.infoIP());
+        //System.out.println(ip1.toString());
+        //System.out.println(ip1.infoIP());
 
     }
 }
